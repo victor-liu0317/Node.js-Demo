9d5a737b9f1dae68525f927a0840c2d1598386c1
Victor Liu <victor-liu0317@users.noreply.github.com>
2016-06-21 10:25:35 +0800

    爬虫示例

---
+++ crawlerDemo.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var url = 'http://www.baidu.com';
+
+http.get(url, function(res){
+	var html = '';
+	res.on('data', function(data){
+		html += data;
+	});
+	res.on('end', function(){
+		console.log(html);
+	});
+
+}).on('error', function(){
+	console.log('出错啦！')
+});
