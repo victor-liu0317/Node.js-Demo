bda7af9ade06cd36bf08adb5256b3727ea1021e4
Victor Liu <victor-liu0317@users.noreply.github.com>
2016-06-23 11:18:43 +0800

    文件读取

---
+++ fs.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+// 异步读取
+fs.readFile('input.txt', function (err, data) {
+   if (err) {
+       return console.error(err);
+   }
+   console.log("异步读取: " + data.toString());
+});
+
+
+console.log("程序执行完毕。");
